5194d3018a77215601eece3e82f3f6b78179e7bb
Jatin Madan <39112299+stark3998@users.noreply.github.com>
2020-07-09 16:05:10 +0530

    Updated Code. Supports individual files and folder

--- doc2txt.py
+++ doc2txt.py
@@ -1,5 +1,43 @@
 import importlib
 
+def convert_folders(folders):
+    print("Converting Folders : ",folders)
+    try:
+        for j in folders:
+            files=glob.glob("{}/*.docx".format(j))
+            print("{} Docx Files found in {} : {}".format(len(files),j,files))
+            try:
+                for i in files:
+                    file_name=i.replace("\\","/").split('/')[-1].split('.')[0]
+                    if "win" in sys.platform:
+                        file_path="\\".join(i.replace("/","\\").split("\\")[:-1])+"\\"
+                    else:
+                        file_path="/".join(i.replace("\\","/").split("/")[:-1])+"/"
+                    doc_text=docx2txt.process(i)
+                    with open(file_path+"\\"+file_name+".txt","w",encoding='utf-8') as f:
+                        f.write(doc_text)
+                    print("Converted {}.docx to {}.txt".format(file_name,file_name))
+            except Exception as e:
+                print("Exception Occured while processing file {} : {}".format(i,ex))
+    except Exception as ex:
+        print("Exception Occured while processing Folder {} : {}".format(j,ex))
+
+def convert_files(files):
+    try:
+        for i in files:
+            file_name=i.replace("\\","/").split('/')[-1].split('.')[0]
+            if "win" in sys.platform:
+                file_path="\\".join(i.replace("/","\\").split("\\")[:-1])+"\\"
+            else:
+                file_path="/".join(i.replace("\\","/").split("/")[:-1])+"/"
+            doc_text=docx2txt.process(i)
+            with open(file_path+"\\"+file_name+".txt","w",encoding='utf-8') as f:
+                f.write(doc_text)
+            print("Converted {}.docx to {}.txt".format(file_name,file_name))
+    except Exception as ex:
+        print("Exception Occured while processing file {} : {}".format(i,ex))
+
+
 def install(package):
     try:
         if "win" in sys.platform:
@@ -23,30 +61,31 @@ for x in listimport:
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-convert", "--convert", help="Path of folders to convert")
+parser.add_argument("-folders", "--folders", help="Path of Folders to convert")
+parser.add_argument("-files", "--files", help="Path of Files to convert")
 args = parser.parse_args()
 
 folders=[]
 try:
-    if args.convert:
-        folders=args.convert.split(',')
+    if args.folders:
+        print(args.folders)
+        folders=args.folders.split(',')
     else:    
         print("No Folders Provided")
 except:
     print("Exception Occured")
 
-print("Converting Folders : ",folders)
+files=[]
+try:
+    if args.files:
+        files=args.files.split(',')
+    else:    
+        print("No Files Provided")
+except:
+    print("Exception Occured")
 
-for i in folders:
-    files=glob.glob("{}/*.docx".format(i))
-    print("{} Docx Files found in {} : {}".format(len(files),i,files))
-    for i in files:
-        file_name=i.replace("\\","/").split('/')[-1].split('.')[0]
-        if "win" in sys.platform:
-            file_path="\\".join(i.replace("/","\\").split("\\")[:-1])+"\\"
-        else:
-            file_path="/".join(i.replace("\\","/").split("/")[:-1])+"/"
-        doc_text=docx2txt.process(i)
-        with open(file_path+"\\"+file_name+".txt","w",encoding='utf-8') as f:
-            f.write(doc_text)
-        print("Converted {}.docx to {}.txt".format(file_name,file_name))
+if folders:
+    convert_folders(folders)
+
+if files:
+    convert_files(files)
